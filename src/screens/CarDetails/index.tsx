823dbac7822decf11b83576b6eabce82ff74a7d4
Luana Sauer <luanasauer23@gmail.com>
2022-01-04 18:00:33 -0300

    componentes_detalhes_acessorios

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { BackButton } from '../../components/BackButton';
+import { Accessory } from '../../components/Accessory';
+import { BackButton } from '../../components/BackButton'; 
 import { ImageSlider } from '../../components/ImageSlider';
 
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
 import {
-     Container,Header,CarImages
+     Container,Header,CarImages,
+     Content,
+     Details,
+     Description,
+     Brand,
+     Name,
+     Rent,
+     Period,
+     Price,
+     About,
+     Accessories
+
 } from './styles';
 
 export function CarDetails() {
@@ -17,7 +36,30 @@ export function CarDetails() {
                     imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']}
                 />
             </CarImages>
-            
-        </Container>
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+                    <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent>
+                </Details>
+                <Accessories>
+                    <Accessory name="380Km/h"  icon={SpeedSvg}/>
+                    <Accessory name="3.2s"  icon={AccelerationSvg}/>
+                    <Accessory name="800 HP"  icon={ForceSvg}/>
+                    <Accessory name="Gasolina"  icon={GasolineSvg}/>
+                    <Accessory name="Auto"  icon={ExchangeSvg}/>
+                    <Accessory name="2 pessoas"  icon={PeopleSvg}/>
+                </Accessories>
+                <About>
+                    Consectetur tempora minima earum unde, 
+                    sit saepe nesciunt magnam iure nisi est ex, ad odio possimus rerum molestiae corrupti perspiciatis aliquid cumque!
+                </About>
+            </Content>
+        </Container> 
     );
 }
